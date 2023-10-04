d3af799ad34ce5e7d3af0f06b4c8bac4b54ed0b0
Fathan Abi <abikarami60@gmail.com>
2023-10-04 12:06:59 +0700

    add bootstrap to input and films page

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -2,18 +2,33 @@
 <html>
 <head>
     <title>Films</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 </head>
 <body>
-    <h1>Films</h1>
-    <ul>
-        @foreach($films as $film)
-            <li>
-                <strong>{{ $film->title }}</strong>
-                ({{ $film->release_year }}) - Directed by {{ $film->director }}
-                <img src="{{ asset('storage/' . $film->image_path) }}" alt="{{ $film->title }}" style="max-width: 200px;">
-            </li>
-        @endforeach
-    </ul>
-    <a href="/input">Add a Film</a>
+    <div class="container">
+        <h1 class="mt-4">Films</h1>
+        <a href="/input" class="btn btn-primary mt-4">Add a Film</a>
+        <ul class="list-group mt-4">
+            @foreach($films as $film)
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <strong>{{ $film->title }}</strong><br>
+                            ({{ $film->release_year }}) - Directed by {{ $film->director }}
+                        </div>
+                        <div class="col-md-6">
+                            <img src="{{ asset('storage/' . $film->image_path) }}" alt="{{ $film->title }}" class="img-fluid">
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+    <!-- Bootstrap JS (optional) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -2,28 +2,44 @@
 <html>
 <head>
     <title>Input Film</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Input Film</h1>
-    @if(session('success'))
-        <p>{{ session('success') }}</p>
-    @endif
-    <form action="/input" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="title">Film Title:</label>
-        <input type="text" name="title" required><br>
+    <div class="container">
+        <h1 class="mt-4">Input Film</h1>
+        @if(session('success'))
+            <div class="alert alert-success mt-3">{{ session('success') }}</div>
+        @endif
+        <form action="/input" method="POST" enctype="multipart/form-data" class="mt-4">
+            @csrf
+            <div class="form-group">
+                <label for="title">Film Title:</label>
+                <input type="text" name="title" class="form-control" required>
+            </div>
 
-        <label for="release_year">Release Year:</label>
-        <input type="number" name="release_year" required><br>
+            <div class="form-group">
+                <label for="release_year">Release Year:</label>
+                <input type="number" name="release_year" class="form-control" required>
+            </div>
 
-        <label for="director">Director:</label>
-        <input type="text" name="director" required><br>
+            <div class="form-group">
+                <label for="director">Director:</label>
+                <input type="text" name="director" class="form-control" required>
+            </div>
 
-        <label for="image">Image:</label>
-        <input type="file" name="image" accept="image/jpeg, image/jpg, image/png" required><br>
+            <div class="form-group">
+                <label for="image">Image:</label>
+                <input type="file" name="image" accept="image/jpeg, image/jpg, image/png" class="form-control-file" required>
+            </div>
 
-        <button type="submit">Submit</button>
-    </form>
-    <a href="/films">View Films</a>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <a href="/films" class="btn btn-secondary mt-3">View Films</a>
+    </div>
+
+    <!-- Bootstrap JS (optional) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
